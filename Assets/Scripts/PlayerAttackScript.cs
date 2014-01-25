f46483318d7212e63d5f297962189d46612a8c6f
Anh Nguyen <anhhn1@gmail.com>
2014-01-24 23:02:09 -0800

    anh's setup

---
+++ Assets/Scripts/PlayerAttackScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerAttackScript : MonoBehaviour {
+
+    public float AttackSpeed = 0.25f;
+    
+    // keeps track of current cooldown
+    private float cooldown;
+
+    void Start()
+    {
+        cooldown = 0f;
+    }
+    
+    void Update()
+    {
+        if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+    }
+    
+    public bool CanAttack
+    {
+        get
+        {
+            return shootCooldown <= 0f;
+        }
+    }
+
+    public void Attack() {
+        
+        if (CanAttack) { // based on cooldown
+            
+            var 
+        }
+    }
+}
