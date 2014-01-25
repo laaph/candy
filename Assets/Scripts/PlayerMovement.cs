1ec7e6f1643f0fddc4d509db7c9534cefd34fe7e
Brian Bartram <bbartram@fablelabs.com>
2014-01-25 15:12:34 -0800

    switched PlayerMovement to use physics and tuned.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,30 +14,42 @@ public class PlayerMovement : MonoBehaviour {
 
 
 	}
+
+	void FixedUpdate() {
+		moveHoriz = "Player" + playerNum + "MoveHoriz";
+		moveVert  = "Player" + playerNum + "MoveVert";
+		
+		
+		//oldPosition = transform.position;
+		float   x =  Input.GetAxis(moveHoriz) * movementSpeed;
+		float   y =  0; // For consistency
+		float   z =  Input.GetAxis(moveVert)  * movementSpeed;
+		rigidbody.AddForce(x, y, z);
+	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		// Temporarily put here so that live updating works - meant to be in Start()
-		moveHoriz = "Player" + playerNum + "MoveHoriz";
-		moveVert  = "Player" + playerNum + "MoveVert";
-
-
-		oldPosition = transform.position;
-		float   x =  Input.GetAxis(moveHoriz) * movementSpeed;
-		float   y =  0; // For consistency
-		float   z =  Input.GetAxis(moveVert)  * movementSpeed;
-
-		// Debug.Log("x = " + x + " y = " + y);
-
-		Vector3 p = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z + z);
-		transform.position = p;
+//		// Temporarily put here so that live updating works - meant to be in Start()
+//		moveHoriz = "Player" + playerNum + "MoveHoriz";
+//		moveVert  = "Player" + playerNum + "MoveVert";
+//
+//
+//		oldPosition = transform.position;
+//		float   x =  Input.GetAxis(moveHoriz) * movementSpeed;
+//		float   y =  0; // For consistency
+//		float   z =  Input.GetAxis(moveVert)  * movementSpeed;
+//
+//		// Debug.Log("x = " + x + " y = " + y);
+//
+//		Vector3 p = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z + z);
+//		transform.position = p;
 	}
 
-	void OnCollisionEnter2D(Collision2D c) {
-		// Debug.Log("Collision info " + c.contacts[0].point);
-		if(c.gameObject.tag == "Wall") {
-			transform.position = oldPosition;
-		}
-	}
+//	void OnCollisionEnter(Collision c) {
+//		 Debug.Log("Collision info " + c.contacts[0].point);
+//		if(c.gameObject.tag == "Wall") {
+//			transform.position = oldPosition;
+//		}
+//	}
 }
