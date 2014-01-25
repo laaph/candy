751e6d81fac1251e16d41ed9c4d8c477df745627
laaph <laaph@laaph.com>
2014-01-25 12:59:59 -0800

    Added labels for attack input

--- Assets/Scripts/PlayerAttackScript.cs
+++ Assets/Scripts/PlayerAttackScript.cs
@@ -1,1 +1,39 @@
-﻿using UnityEngine;public class PlayerAttackScript : MonoBehaviour {    // inspector variables    public float attackSpeed = 1.2f;    //public Transform         // keeps track of current cooldown    private float cooldown;    void Start() {        cooldown = 0f;    }        void Update() {                if (cooldown > 0) {            cooldown -= Time.deltaTime;        }                bool attackPressed = Input.GetButtonDown("Fire1");        attackPressed |= Input.GetButtonDown("Fire2");                if (attackPressed && cooldown <= 0f) {            // to do        }    }}
+﻿using UnityEngine;
+
+public class PlayerAttackScript : MonoBehaviour {
+
+    // inspector variables
+    public float attackSpeed = 1.2f;
+    //public Transform 
+    
+    // keeps track of current cooldown
+    private float cooldown;
+
+    void Start() {
+        cooldown = 0f;
+    }
+    
+    void Update() {
+        
+        if (cooldown > 0) {
+            cooldown -= Time.deltaTime;
+        }
+        
+		// attackX and attackY give direction of attack
+		// button let's us know to attack
+		// there is also Player2Fire and associated input
+		// Movement is handled with Player1MoveHoriz and Player1MoveVert
+
+        bool attackPressed = Input.GetButtonDown("Player1Fire");
+		float attackX = Input.GetAxis("Player1ShootHoriz");
+		float attackY = Input.GetAxis("Player1ShootVert");
+
+		// Not sure what you aer doing here, let me know if I should add another button input.
+
+        attackPressed |= Input.GetButtonDown("Fire2");
+        
+        if (attackPressed && cooldown <= 0f) {
+            // to do
+        }
+    }
+}
