d3fb73f1256c4aa1b93283f9f4fc0d2fcd2f155c
laaph <laaph@laaph.com>
2014-01-25 10:09:43 -0800

    Added follow script for cape

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public Transform followTarget;
+	Vector3 startLocation;
+	// Use this for initialization
+	void Start () {
+		startLocation = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void FixedUpdate() {
+		transform.position = followTarget.position + startLocation;
+	}
+}
