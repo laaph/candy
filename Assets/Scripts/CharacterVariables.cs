e2cf3ac0942a99a8afa2ed4330cb23c0f08510fd
laaph <laaph@laaph.com>
2014-01-25 13:40:47 -0800

    Added CharacterVariables

---
+++ Assets/Scripts/CharacterVariables.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public int team = 1;
+
+	public int hp;
+	public int maxHp;
+
+	public int   attackDamage;
+	public float attackRadius;
+	public float attackArc;
+	public float attackSpeed;
+
+	public float moveSpeed;
+
+		
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
