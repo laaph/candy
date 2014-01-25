86fd2ad7cef7cc89ad8541dac591beb7a1800e2a
laaph <laaph@laaph.com>
2014-01-25 11:45:11 -0800

    Added simple collision detection

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	public float movementSpeed = 0.1f;
+	Vector3 oldPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -12,13 +13,21 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		oldPosition = transform.position;
 		float   x =  Input.GetAxis("Player1MoveHoriz") * movementSpeed;
 		float   y =  0; // For consistency
 		float   z =  Input.GetAxis("Player1MoveVert")  * movementSpeed;
 
-		//Debug.Log("x = " + x + " y = " + y);
+		// Debug.Log("x = " + x + " y = " + y);
 
 		Vector3 p = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z + z);
 		transform.position = p;
+	}
+
+	void OnCollisionEnter2D(Collision2D c) {
+		// Debug.Log("Collision info " + c.contacts[0].point);
+		if(c.gameObject.tag == "Wall") {
+			transform.position = oldPosition;
+		}
 	}
 }
